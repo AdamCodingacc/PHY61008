5eaea30b4a81ada542b76d9020328d5ccf8174d7
AdamCodingacc <72269700+AdamCodingacc@users.noreply.github.com>
2021-10-06 16:49:04 +0100

    COM & all planets added
    COM and COV added plus rest of the planets plus converted to AU

--- three_body_work/main.f90
+++ three_body_work/main.f90
@@ -5,17 +5,22 @@ PROGRAM three_body
     !Declare variables
     IMPLICIT NONE
     !x,y,z position, velocity, mass, initial acceln, new acceln for 3 bodies
-    DOUBLEPRECISION :: r(1:3, 1:3), v(1:3, 1:3), M(1:3), a_0(1:3, 1:3), a_1(1:3, 1:3)
-    DOUBLEPRECISION :: s_sq(1:3, 1:3), dt, G, time, GPE(1:3), absv_sq(1:3), E_0, E_1
+    DOUBLEPRECISION :: r(1:3, 1:7), v(1:3, 1:7), M(1:7), a_0(1:3, 1:7), a_1(1:3, 1:7)
+    DOUBLEPRECISION :: COM(1:3), COV(1:3), s_sq(1:7, 1:7), dt, G, time, GPE(1:7), absv_sq(1:7), E_0, E_1, Mtot, AU
     INTEGER :: n, i, j, k !Define indexing integers
 
 !Initialize variables
 G = 6.67e-11
-n = 3
+n = 6
 dt = 100
+AU = 1.496e11
 M(1) = 1.99e30
 M(2) = 5.97e24
 M(3) = 1.898e27
+M(4) = 6.4171e23
+M(5) = 5.683e26
+M(6) = 8.681e25
+M(7) = 1.024e26
 
 !Empty array variables
 s_sq = 0
@@ -26,24 +31,69 @@ E_0 = 0
 E_1 = 0
 a_0 = 0
 a_1 = 0
+COM = 0
+COV = 0
 
 !Initialize starting positions
 r(:,1) = 0 !System centred around Sun
-r(1,2) = 1.496e11 * (1.)
-r(2,2) = 1.496e11 * cos(1.)
+r(1,2) = AU * sin(1.) * -1.
+r(2,2) = AU * cos(1.)
 r(3,2) = 0
-r(1,3) = 5.2 * 1.496e11 * sin(1.)
-r(2,3) = 5.2 * 1.496e11 * cos(1.)
+r(1,3) = 5.2 * AU * sin(1.) * -1.
+r(2,3) = 5.2 * AU * cos(1.)
 r(3,3) = 0
+r(1,4) = 1.52 * AU * sin(1.) * -1.
+r(2,4) = 1.52 * AU * cos(1.)
+r(3,4) = 0
+r(1,5) = 9.583 * AU * sin(1.) * -1.
+r(2,5) = 9.583 * AU * cos(1.)
+r(3,5) = 0
+r(1,6) = 19.201 * AU * sin(1.) * -1.
+r(2,6) = 19.201 * AU * cos(1.)
+r(3,6) = 0
+r(1,7) = 30.07 * AU * sin(1.) * -1.
+r(2,7) = 30.07 * AU * cos(1.)
+r(3,7) = 0
 
 !Initialize starting velocities
 v(:,1) = 0
-v(1,2) = 29780 * cos(1.)
-v(2,2) = 29780 * sin(1.)
+v(1,2) = 29780 * -1. * cos(1.)
+v(2,2) = 29780 * -1. * sin(1.)
 v(3,2) = 0
-v(1,3) = 13060 * cos(1.)
-v(2,3) = 13060 * sin(1.)
+v(1,3) = 13060 * -1. * cos(1.)
+v(2,3) = 13060 * -1. * sin(1.)
 v(3,3) = 0
+v(1,4) = 24070 * -1. * cos(1.)
+v(2,4) = 24070 * -1. * sin(1.)
+v(3,4) = 0
+v(1,5) = 9680 * -1. * cos(1.)
+v(2,5) = 9680 * -1. * sin(1.)
+v(3,5) = 0
+v(1,6) = 6800 * -1. * cos(1.)
+v(2,6) = 6800 * -1. * sin(1.)
+v(3,6) = 0
+v(1,7) = 5430 * -1. * cos(1.)
+v(2,7) = 5430 * -1. * sin(1.)
+v(3,7) = 0
+
+DO i = 1, n-1
+        DO k = 1,3
+                COM(k) = COM(k) + r(k,i)*M(i)
+                COV(k) = Cov(k) + v(k,i)*M(i)
+        END DO
+Mtot = Mtot + M(i)
+END DO
+
+!Correct for COM and COV
+DO i = 1,n
+    r(:,i) = r(:,i) - COM/Mtot
+    v(:,i) = v(:,i) - COV/Mtot
+
+    DO k = 1,3
+        absv_sq(i) = absv_sq(i) + v(k,i)**2
+    END DO
+END DO
+
 
 !Calculate initial conditions and thus initial acceleration
 DO i = 1,n
@@ -66,18 +116,15 @@ DO i = 1,n
 GPE = GPE*0.5
 
 DO i = 1,n
-    DO k = 1,3
-        absv_sq(i) = absv_sq(i) + v(k,i)**2
-    END DO
     E_0 = E_0 + 0.5*M(i)*absv_sq(i) + GPE(i)
 END DO
 
 WRITE(6,*) "Initial Conditions"
-WRITE(6,*) "Position Vectors xyz (m)"
+WRITE(6,*) "Position Vectors xyz (AU)"
 WRITE(6,*) ""
 !Print initial coordinates of all bodies
 DO i = 1,n
-    WRITE(6,*) r(:,i)
+    WRITE(6,*) (r(:,i)/AU)
 END DO
 
 WRITE(6,*) ""
@@ -133,11 +180,11 @@ WRITE(6,*) E_0
 
 WRITE(6,*) ""
 WRITE(6,*) "Final Conditions"
-WRITE(6,*) "Position Vectors xyz (m)"
+WRITE(6,*) "Position Vectors xyz (AU)"
 WRITE(6,*) ""
 !Print final coordinates of all bodies
 DO i = 1,n
-    WRITE(6,*) r(:,i)
+    WRITE(6,*) (r(:,i)/AU)
 END DO
 
 WRITE(6,*) ""
