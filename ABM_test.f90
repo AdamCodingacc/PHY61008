9cf2087c6fc6ef1c5b94a0478f80faae5236bcb7
AdamCodingacc <72269700+AdamCodingacc@users.noreply.github.com>
2021-10-23 16:05:25 +0100

    Broken Predictor

--- ABM_test.f90
+++ ABM_test.f90
@@ -4,7 +4,7 @@ PROGRAM solar_sim
     !Declare variables
     IMPLICIT NONE
     !x,y,z position, velocity, mass, initial acceln, new acceln for 7 bodies
-    DOUBLEPRECISION :: r(1:3, 1:7), M(1:7), v(1:3, 1:7, -3:1),  a(1:3, 1:7, -3:1)
+    DOUBLEPRECISION :: r(1:3, 1:7, 0:1), M(1:7), v(1:3, 1:7, -3:1),  a(1:3, 1:7, -3:1)
     DOUBLEPRECISION :: COM(1:3), COV(1:3), s_sq(1:7, 1:7), s(1:7, 1:7), GPE(1:7), absv_sq(1:7), dist(1:3, 1:7, 1:7)
     DOUBLEPRECISION :: E_0, E_1, Mtot, AU, dt, G, time
     INTEGER :: num_yr, n, i, j, k, z, b, stepno !Define indexing integers
@@ -13,7 +13,7 @@ PROGRAM solar_sim
 !Initialize variables
 G = 6.67e-11
 n = 7
-dt = 1
+dt = 100
 AU = 1.496e11
 M(1) = 1.99e30
 M(2) = 5.97e24
@@ -38,25 +38,25 @@ COM = 0
 COV = 0
 
 !Initialize starting positions
-r(:,1) = 0 !System centred around Sun
-r(1,2) = AU * sin(1.) * -1.
-r(2,2) = AU * cos(1.)
-r(3,2) = 0
-r(1,3) = 5.2 * AU * sin(1.) * -1.
-r(2,3) = 5.2 * AU * cos(1.)
-r(3,3) = 0
-r(1,4) = 1.52 * AU * sin(1.) * -1.
-r(2,4) = 1.52 * AU * cos(1.)
-r(3,4) = 0
-r(1,5) = 9.583 * AU * sin(1.) * -1.
-r(2,5) = 9.583 * AU * cos(1.)
-r(3,5) = 0
-r(1,6) = 19.201 * AU * sin(1.) * -1.
-r(2,6) = 19.201 * AU * cos(1.)
-r(3,6) = 0
-r(1,7) = 30.07 * AU * sin(1.) * -1.
-r(2,7) = 30.07 * AU * cos(1.)
-r(3,7) = 0
+r(:,1,0) = 0 !System centred around Sun
+r(1,2,0) = AU * sin(1.) * -1.
+r(2,2,0) = AU * cos(1.)
+r(3,2,0) = 0
+r(1,3,0) = 5.2 * AU * sin(1.) * -1.
+r(2,3,0) = 5.2 * AU * cos(1.)
+r(3,3,0) = 0
+r(1,4,0) = 1.52 * AU * sin(1.) * -1.
+r(2,4,0) = 1.52 * AU * cos(1.)
+r(3,4,0) = 0
+r(1,5,0) = 9.583 * AU * sin(1.) * -1.
+r(2,5,0) = 9.583 * AU * cos(1.)
+r(3,5,0) = 0
+r(1,6,0) = 19.201 * AU * sin(1.) * -1.
+r(2,6,0) = 19.201 * AU * cos(1.)
+r(3,6,0) = 0
+r(1,7,0) = 30.07 * AU * sin(1.) * -1.
+r(2,7,0) = 30.07 * AU * cos(1.)
+r(3,7,0) = 0
 
 !Initialize starting velocities
 v(:,1,0) = 0
@@ -80,14 +80,14 @@ v(2,7,0) = 5430 * -1. * sin(1.)
 v(3,7,0) = 0
 
 DO i = 1, n-1
-    COM(:) = COM(:) + r(:,i)*M(i)
+    COM(:) = COM(:) + r(:,i,0)*M(i)
     COV(:) = COV(:) + v(:,i,0)*M(i)
     Mtot = Mtot + M(i)
 END DO
 
 !Correct for COM and COV
 DO i = 1,n
-    r(:,i) = r(:,i) - COM/Mtot
+    r(:,i,0) = r(:,i,0) - COM/Mtot
     v(:,i,0) = v(:,i,0) - COV/Mtot
 
     DO k = 1,3
@@ -102,13 +102,13 @@ DO i = 1,n
             IF (i == j) CYCLE
 
                 !Find absolute distance squared between bodies i & j
-                s_sq(i,j) = ((r(1,j) - r(1,i))**2 + (r(2,j) - r(2,i))**2 + (r(3,j) - r(3,i))**2)
+                s_sq(i,j) = ((r(1,j,0) - r(1,i,0))**2 + (r(2,j,0) - r(2,i,0))**2 + (r(3,j,0) - r(3,i,0))**2)
 
                 GPE(i) = GPE(i) - G*M(i)*M(j)/sqrt(s_sq(i,j))
 
 
                 !Need + so each object does not overwrite previous
-                a(:,i,0) = a(:,i,0) + (G * M(j) * (r(:,j) - r(:,i)) * (s_sq(i,j)**-1.5))
+                a(:,i,0) = a(:,i,0) + (G * M(j) * (r(:,j,0) - r(:,i,0)) * (s_sq(i,j)**-1.5))
 
             END DO
         END DO
@@ -125,7 +125,7 @@ WRITE(6,*) "Position Vectors xyz (AU)"
 WRITE(6,*) ""
 !Print initial coordinates of all bodies
 DO i = 1,n
-    WRITE(6,*) (r(:,i)/AU)
+    WRITE(6,*) (r(:,i,0)/AU)
 END DO
 
 WRITE(6,*) ""
@@ -173,7 +173,7 @@ DO k = 0,3
 
     !Find new position, r, after time-step
     !Comes at the start of the array to get the new position before other variables updated
-    r = r + v(:,:,-1)*dt + 0.5*a(:,:,-1)*dt**2
+    r(:,:,0) = r(:,:,0) + v(:,:,0)*dt + 0.5*a(:,:,0)*dt**2
 
     !Shift previous values down array
     DO z = -3,0
@@ -188,11 +188,11 @@ DO k = 0,3
         IF (i == j) CYCLE
 
             !Find absolute distance squared between bodies i & j
-            s_sq(i,j) = ((r(1,j) - r(1,i))**2 + (r(2,j) - r(2,i))**2 + (r(3,j) - r(3,i))**2)
+            s_sq(i,j) = ((r(1,j,0) - r(1,i,0))**2 + (r(2,j,0) - r(2,i,0))**2 + (r(3,j,0) - r(3,i,0))**2)
 
 
             !Find acceleration on object i in each dimension
-            a(:,i,0) = a(:,i,0) + G * M(j) * (r(:,j) - r(:,i))*(s_sq(i,j)**-1.5)
+            a(:,i,0) = a(:,i,0) + G * M(j) * (r(:,j,0) - r(:,i,0))*(s_sq(i,j)**-1.5)
 
 
         END DO
@@ -209,7 +209,7 @@ WRITE(6,*) "Position Vectors xyz (AU)"
 WRITE(6,*) ""
 !Print initial coordinates of all bodies
 DO i = 1,n
-    WRITE(6,*) (r(:,i)/AU)
+    WRITE(6,*) (r(:,i,0)/AU)
 END DO
 
 WRITE(6,*) ""
@@ -217,48 +217,55 @@ WRITE(6,*) "Velocity xyz (ms^-1)"
 WRITE(6,*) ""
 !Print initial coordinates of all bodies
 DO i = 1,n
-    WRITE(6,*) v(:,i,0)
+    DO k = -3,1
+        WRITE(6,*) v(:,i,k)
+    END DO
 END DO
 
 WRITE(6,*) ""
 WRITE(6,*) "Acceleration xyz (ms^-2)"
 WRITE(6,*) ""
 DO i = 1,n
-    WRITE(6,*) a(:,i,0)
+    DO k = -3,1
+        WRITE(6,*) a(:,i,k)
+    END DO
 END DO
 
 !Increase time-step for predictor
-dt = 1000
+!dt = 1000
 DO
-    !Predict position using ABM predictor
-    r = r + (dt/24)*(-9*v(:,:,-3) + 37*v(:,:,-2) - 59*v(:,:,-1) + 55*v(:,:,0))
-    !Shift previous values down array
-    DO z = -3,0
-        a(:,:,z) = a(:,:,z+1)
-        v(:,:,z) = v(:,:,z+1)
-    END DO
     s_sq = 0
     dist = 0
+    a(:,:,1) = 0
 
+    !Predict position using ABM predictor
+    r(:,:,1) = r(:,:,0) + ((dt/24.)*(-9.*v(:,:,-3) + 37.*v(:,:,-2) - 59.*v(:,:,-1) + 55.*v(:,:,0)))
 
     DO i = 1,n
         DO j = 1,n
         !skip loop if i & j same (no force exerted by body on self)
         IF (i == j) CYCLE
-            dist(:, i, j) = r(:, j) - r(:, i)
+            dist(:, i, j) = r(:,j,1) - r(:,i,1)
 
             !Find absolute distance squared between bodies i & j
-            s_sq(i,j) = ((r(1,j) - r(1,i))**2 + (r(2,j) - r(2,i))**2 + (r(3,j) - r(3,i))**2)
+            s_sq(i,j) = ((r(1,j,1) - r(1,i,1))**2 + (r(2,j,1) - r(2,i,1))**2 + (r(3,j,1) - r(3,i,1))**2)
 
             !Find acceleration on object i in each dimension
-            a(:,i,0) = a(:,i,0) + G * M(j) * (r(:,j) - r(:,i))*(s_sq(i,j)**-1.5)
+            a(:,i,1) = a(:,i,1) + G * M(j) * (r(:,j,1) - r(:,i,1))*(s_sq(i,j)**-1.5)
 
 
         END DO
     END DO
 
     !Predict velocity using ABM predictor
-    v(:,:,1) = v(:,:,0) + (dt/24)*(-9*a(:,:,-3) + 37*a(:,:,-2) - 59*a(:,:,-1) + 55*a(:,:,0))
+    v(:,:,1) = v(:,:,0) + ((dt/24.)*(-9.*a(:,:,-3) + 37.*a(:,:,-2) - 59.*a(:,:,-1) + 55.*a(:,:,0)))
+
+    !Shift previous values down array
+    v(:,:,-3:0) = v(:,:,-2:1)
+    a(:,:,-3:0) = a(:,:,-2:1)
+    r(:,:,0) = r(:,:,1)
+
+
 
     !Write every 250th step to log files
     IF ((MOD(stepno, 500) == 0) .and. (lg == 'y')) THEN
@@ -295,14 +302,25 @@ WRITE(6,*) "Position Vectors xyz (AU)"
 WRITE(6,*) ""
 !Print final coordinates of all bodies
 DO i = 1,n
-    WRITE(6,*) (r(:,i)/AU)
+    WRITE(6,*) (r(:,i,0)/AU)
+END DO
+
+WRITE(6,*) ""
+WRITE(6,*) "Velocity xyz (ms^-1)"
+WRITE(6,*) ""
+DO i = 1,n
+    DO k = -3,1
+        WRITE(6,*) v(:,i,k)
+    END DO
 END DO
 
 WRITE(6,*) ""
 WRITE(6,*) "Acceleration xyz (ms^-2)"
 WRITE(6,*) ""
 DO i = 1,n
-    WRITE(6,*) a(:,i,0)
+    DO k = -3,1
+        WRITE(6,*) a(:,i,k)
+    END DO
 END DO
 
 
@@ -313,7 +331,7 @@ absv_sq = 0
 DO i = 1,n
         DO j = 1,n
             IF (i == j) CYCLE
-                s_sq(i,j) = ((r(1,j) - r(1,i))**2 + (r(2,j) - r(2,i))**2 + (r(3,j) - r(3,i))**2)
+                s_sq(i,j) = ((r(1,j,0) - r(1,i,0))**2 + (r(2,j,0) - r(2,i,0))**2 + (r(3,j,0) - r(3,i,0))**2)
 
                 !Sum calculated GPE
                 GPE(i) = GPE(i) - G*M(i)*M(j)/sqrt(s_sq(i,j))
