7fee0eb3f3fb87458fd0352ec28b6e4e68f415e1
AdamCodingacc <72269700+AdamCodingacc@users.noreply.github.com>
2022-02-27 17:53:31 +0000

    Multi particle start
    Started implementation for multi particle timestep

--- Python/Orbit_Plotter.py
+++ Python/Orbit_Plotter.py
@@ -4,6 +4,7 @@ import numpy as np
 import csv
 
 AU = 1.496e13 #in cm
+pc = 3.086e18
 
 t = np.array([]) #Create empty array for time
 xsun = ysun = zsun = np.array([]) #Create arrays for Sun position in each dimension
@@ -15,7 +16,8 @@ xura = yura = zura = np.array([]) #Arrays for Uranus
 xnep = ynep = znep = np.array([]) #Arrays for Neptune
 
 
-file = open('E:/Uni stuff not full of origin files/4th Year/Project/GitHub/PHY61008/Sun_Motion.csv')
+#file = open('E:/Uni stuff not full of origin files/4th Year/Project/GitHub/PHY61008/Sun_Motion.csv')
+file = open('E:/Uni stuff not full of origin files/4th Year/Project/Sun_Motion.csv')
 look = csv.reader(file,delimiter = ',')
 for row in look:
     xsun = np.append(xsun,float(row[1]))
@@ -23,7 +25,8 @@ for row in look:
     zsun = np.append(zsun,float(row[3]))
 file.close()
 
-file = open('E:/Uni stuff not full of origin files/4th Year/Project/GitHub/PHY61008/Earth_Motion.csv')
+#file = open('E:/Uni stuff not full of origin files/4th Year/Project/GitHub/PHY61008/Earth_Motion.csv')
+file = open('E:/Uni stuff not full of origin files/4th Year/Project/Earth_Motion.csv')
 look = csv.reader(file,delimiter = ',')
 for row in look:
     t = np.append(t,float(row[0]))
@@ -34,7 +37,8 @@ file.close()
 
 
 
-file = open('E:/Uni stuff not full of origin files/4th Year/Project/GitHub/PHY61008/Jupiter_Motion.csv')
+#file = open('E:/Uni stuff not full of origin files/4th Year/Project/GitHub/PHY61008/Jupiter_Motion.csv')
+file = open('E:/Uni stuff not full of origin files/4th Year/Project/Jupiter_Motion.csv')
 look = csv.reader(file,delimiter = ',')
 for row in look:
     xjup = np.append(xjup,float(row[1]))
@@ -42,7 +46,8 @@ for row in look:
     zjup = np.append(zjup,float(row[3]))
 file.close()
 
-file = open('E:/Uni stuff not full of origin files/4th Year/Project/GitHub/PHY61008/Mars_Motion.csv')
+#file = open('E:/Uni stuff not full of origin files/4th Year/Project/GitHub/PHY61008/Mars_Motion.csv')
+file = open('E:/Uni stuff not full of origin files/4th Year/Project/Mars_Motion.csv')
 look = csv.reader(file,delimiter = ',')
 for row in look:
     xmars = np.append(xmars,float(row[1]))
@@ -50,7 +55,8 @@ for row in look:
     zmars = np.append(zmars,float(row[3]))
 file.close()
 
-file = open('E:/Uni stuff not full of origin files/4th Year/Project/GitHub/PHY61008/Saturn_Motion.csv')
+#file = open('E:/Uni stuff not full of origin files/4th Year/Project/GitHub/PHY61008/Saturn_Motion.csv')
+file = open('E:/Uni stuff not full of origin files/4th Year/Project/Saturn_Motion.csv')
 look = csv.reader(file,delimiter = ',')
 for row in look:
     xsat = np.append(xsat,float(row[1]))
@@ -58,7 +64,8 @@ for row in look:
     zsat = np.append(zsat,float(row[3]))
 file.close()
 
-file = open('E:/Uni stuff not full of origin files/4th Year/Project/GitHub/PHY61008/Uranus_Motion.csv')
+#file = open('E:/Uni stuff not full of origin files/4th Year/Project/GitHub/PHY61008/Uranus_Motion.csv')
+file = open('E:/Uni stuff not full of origin files/4th Year/Project/Uranus_Motion.csv')
 look = csv.reader(file,delimiter = ',')
 for row in look:
     xura = np.append(xura,float(row[1]))
@@ -66,7 +73,8 @@ for row in look:
     zura = np.append(zura,float(row[3]))
 file.close()
 
-file = open('E:/Uni stuff not full of origin files/4th Year/Project/GitHub/PHY61008/Neptune_Motion.csv')
+#file = open('E:/Uni stuff not full of origin files/4th Year/Project/GitHub/PHY61008/Neptune_Motion.csv')
+file = open('E:/Uni stuff not full of origin files/4th Year/Project/Neptune_Motion.csv')
 look = csv.reader(file,delimiter = ',')
 for row in look:
     xnep = np.append(xnep,float(row[1]))
@@ -85,12 +93,12 @@ image.set_ylim(-40, 40)
 image.set_zlim(-40, 40)
 """
 
-plt.plot(xsun/AU, ysun/AU, zsun/AU, '-', c='yellow')
-plt.plot(xear/AU, year/AU, zear/AU,'-', c='b')
-plt.plot(xjup/AU, yjup/AU, zjup/AU, '-', c='saddlebrown')
-plt.plot(xmars/AU, ymars/AU, zmars/AU,'-', c='r')
-plt.plot(xsat/AU, ysat/AU, zsat/AU,'-', c='magenta')
-plt.plot(xura/AU, yura/AU, zura/AU,'-', c='cyan')
-plt.plot(xnep/AU, ynep/AU, znep/AU,'-', c='black')
+plt.plot(xsun*pc/AU, ysun*pc/AU, zsun*pc/AU, '-', c='yellow')
+plt.plot(xear*pc/AU, year*pc/AU, zear*pc/AU,'-', c='b')
+plt.plot(xjup*pc/AU, yjup*pc/AU, zjup*pc/AU, '-', c='saddlebrown')
+plt.plot(xmars*pc/AU, ymars*pc/AU, zmars*pc/AU,'-', c='r')
+plt.plot(xsat*pc/AU, ysat*pc/AU, zsat*pc/AU,'-', c='magenta')
+plt.plot(xura*pc/AU, yura*pc/AU, zura*pc/AU,'-', c='cyan')
+plt.plot(xnep*pc/AU, ynep*pc/AU, znep*pc/AU,'-', c='black')
 
 plt.show()
--- multi_ptcl.f90
+++ multi_ptcl.f90
@@ -4,16 +4,18 @@ PROGRAM solar_sim
     !Declare variables
     IMPLICIT NONE
     !x,y,z position, velocity, mass, initial acceln, new acceln for 7 bodies
-    DOUBLEPRECISION :: r(1:3, 1:7, 0:2), M(1:7), v(1:3, 1:7, -6:2),  a(1:3, 1:7, -6:2), a_int(1:3, 1:7, 1:2), v_int(1:3, 1:7, 1:2)
-    DOUBLEPRECISION :: COM(1:3), COV(1:3), s_sq(1:7, 1:7), s(1:7, 1:7), GPE(1:7), absv_sq(1:7), dist(1:3, 1:7, 1:7)
-    DOUBLEPRECISION :: E_0, E_1, E_check, Mtot, AU, dt, G, time, num_yr, RelErr, Small, errcoeff, KE, pot
-    INTEGER :: n, i, j, k, z, stepno, counter !Define indexing integers
+    DOUBLEPRECISION :: r(1:3, 1:7, 0:2), v(1:3, 1:7, -6:2),  a(1:3, 1:7, -6:2), a_int(1:3, 1:7, 1:2), v_int(1:3, 1:7, 1:2)
+    DOUBLEPRECISION :: COM(1:3), COV(1:3), s_sq(1:7, 1:7), s(1:7, 1:7), absv_sq(1:7), dist(1:3, 1:7, 1:7), rtemp(1:3,1:7)
+    DOUBLEPRECISION, DIMENSION(1:7) :: M, GPE, dt, deltat
+    DOUBLEPRECISION :: E_0, E_1, E_check, Mtot, AU, dtmin, G, time, num_yr, RelErr, Small, errcoeff, KE, pot
+    INTEGER :: n, i, j, k, z, stepno, counter(1:7) !Define indexing integers
     CHARACTER(LEN = 1) :: lg
+    LOGICAL, DIMENSION(1:7) :: accdo
 
 !Initialize variables
 G = 6.67e-11
 n = 7
-dt = 1.
+dtmin = 1.
 AU = 1.496e11
 RelErr = 5.e-13
 Small = 1.e-7
@@ -201,12 +203,12 @@ DO k = 0,5
 
     !Find new position, r, after time-step
     !Comes at the start of the array to get the new position before other variables updated
-    r(:,:,0) = r(:,:,0) + v(:,:,0)*dt + 0.5*a(:,:,0)*dt**2
+    r(:,:,0) = r(:,:,0) + v(:,:,0)*dtmin + 0.5*a(:,:,0)*dtmin**2
 
     !Shift previous values down arrays
-    DO z = -3,-1
-        a(:,:,z) = a(:,:,z+1)
-        v(:,:,z) = v(:,:,z+1)
+    DO i = -3,-1
+        a(:,:,i) = a(:,:,i+1)
+        v(:,:,i) = v(:,:,i+1)
     END DO
 
     DO i = 1,n
@@ -226,153 +228,150 @@ DO k = 0,5
     END DO
 
     !Find new velocities after time-step
-    v(:,:,0) = v(:,:,0) + 0.5*(a(:,:,-1) + a(:,:,0))*dt
+    v(:,:,0) = v(:,:,0) + 0.5*(a(:,:,-1) + a(:,:,0))*dtmin
 
     !Update current time
-    time = time + dt
+    time = time + dtmin
 END DO
 
 !==================================================================================================================
-
+!Ensure that all bodies are done in the first iteration
+deltat(:) = dtmin
+dt(:) = dtmin
 !ABM Method
 DO
+    DO z = 1,n
+        !clear temporary interpolated values
 
-    !Print current Earth distance
-    !WRITE(6,*) (r(:,2,0) /AU)
+        !skip bodies when their individual timestep has not been reached
+        IF (deltat(z) = dt(z)) CYCLE
 
-    !Clear predicted acceleration to not factor into calculation
-    a(:,:,1) = 0
+        !Clear predicted acceleration to not factor into calculation
+        a(:,z,1) = 0
 
-    !Predict position and velocity using ABM predictor
-    r(:,:,1) = r(:,:,0) + ((dt/24.) * (-9.*v(:,:,-3) + 37.*v(:,:,-2) - 59.*v(:,:,-1) + 55.*v(:,:,0)))
-    v(:,:,1) = v(:,:,0) + ((dt/24.) * (-9.*a(:,:,-3) + 37.*a(:,:,-2) - 59.*a(:,:,-1) + 55.*a(:,:,0)))
+        !Predict position and velocity using ABM predictor
+        r(:,z,1) = r(:,z,0) + ((dt(z)/24.) * (-9.*v(:,z,-3) + 37.*v(:,z,-2) - 59.*v(:,z,-1) + 55.*v(:,z,0)))
+        v(:,z,1) = v(:,z,0) + ((dt(z)/24.) * (-9.*a(:,z,-3) + 37.*a(:,z,-2) - 59.*a(:,z,-1) + 55.*a(:,z,0)))
 
-    !Calculate acceleration for predicted position
-    DO i = 1,n
-        DO j = 1,n
-        IF (i == j) CYCLE
-            !Calculate absolute distance in each direction for logging files
-            dist(:, i, j) = r(:,j,1) - r(:,i,1)
-
-            !calculate square of absolute distance
-            s_sq(i,j) = ((r(1,j,1) - r(1,i,1))**2 + (r(2,j,1) - r(2,i,1))**2 + (r(3,j,1) - r(3,i,1))**2)
-
-            !Calculate new acceleration
-            a(:,i,1) = a(:,i,1) + G * M(j) * (r(:,j,1) - r(:,i,1))*(s_sq(i,j)**-1.5)
+        DO i =1,n
+            !skip interpolation for body acceleration is being calculated for
+            IF (i==z) CYCLE
+            !interpolate position from most recently updated position
+            rtemp(:,i) = r(:,i,0) + v(:,i,0)*deltat(i) + 0.5*a(:,i,0)*deltat(i)**2
         END DO
-    END DO
 
+        !use predicted position for body acceleration is being calculated for
+        rtemp(:,z) = r(:,z,0)
 
-
-    !ABM corrector
-    r(:,:,2) = r(:,:,0) + ((dt/24) * (v(:,:,-2) - 5.*v(:,:,-1) + 19.*v(:,:,0) + 9.*v(:,:,1)))
-    v(:,:,2) = v(:,:,0) + ((dt/24) * (a(:,:,-2) - 5.*a(:,:,-1) + 19.*a(:,:,0) + 9.*a(:,:,1)))
-
-
-    !Recalculate acceleration for corrected position
-    a(:,:,2) = 0
-    DO i = 1,n
-        DO j = 1,n
-        IF (i == j) CYCLE
-            dist(:, i, j) = r(:,j,2) - r(:,i,2)
-
-            s_sq(i,j) = ((r(1,j,2) - r(1,i,2))**2 + (r(2,j,2) - r(2,i,2))**2 + (r(3,j,2) - r(3,i,2))**2)
-
-            !Calculate corrected acceleration
-            a(:,i,2) = a(:,i,2) + G * M(j) * (r(:,j,2) - r(:,i,2))*(s_sq(i,j)**-1.5)
-        END DO
-    END DO
-
-    !Shift previous values down arrays
-    DO z = -6,0
-        a(:,:,z) = a(:,:,z+1)
-        v(:,:,z) = v(:,:,z+1)
-    END DO
-
-    !Set corrected values as current position, velocity and acceleration
-    r(:,:,0) = r(:,:,2)
-    v(:,:,0) = v(:,:,2)
-    a(:,:,0) = a(:,:,2)
-
-
-
-GPE = 0
-absv_sq = 0
-DO i = 1,n
+        !Calculate acceleration for predicted position
+        DO i = 1,n
             DO j = 1,n
-            !skip loop if i & j same (no force exerted by body on self)
             IF (i == j) CYCLE
-                GPE(i) = GPE(i) - G*M(i)*M(j)/sqrt(s_sq(i,j))
+                !calculate square of absolute distance
+                s_sq(i,j) = ((r(1,j,1) - r(1,i,1))**2 + (r(2,j,1) - r(2,i,1))**2 + (r(3,j,1) - r(3,i,1))**2)
+
+                !Calculate new acceleration
+                a(:,i,1) = a(:,i,1) + G * M(j) * (r(:,j,1) - r(:,i,1))*(s_sq(i,j)**-1.5)
             END DO
         END DO
-GPE = GPE*0.5
-
-DO i = 1,n
-    E_check = E_check + 0.5*M(i)*absv_sq(i) + GPE(i)
-END DO
-!WRITE(6,*) "Current Energy error"
-!WRITE(6,*) E_0 - E_check
-
-!Write every 100th step energy error to a log file
-IF ((MOD(stepno, 250) == 0)) THEN
-    WRITE(11,*) time, ',', E_0 - E_check, ',', E_check / E_0
-END IF
 
 
 
+        !ABM corrector
+        r(:,:,2) = r(:,:,0) + ((dtmin/24) * (v(:,:,-2) - 5.*v(:,:,-1) + 19.*v(:,:,0) + 9.*v(:,:,1)))
+        v(:,:,2) = v(:,:,0) + ((dtmin/24) * (a(:,:,-2) - 5.*a(:,:,-1) + 19.*a(:,:,0) + 9.*a(:,:,1)))
 
 
-    !Ensure enough data points are stored
-    IF (counter > 6) THEN
-        !Check if error from predictor-corrector is small enough
-        IF (errcoeff * MAXVAL(ABS(r(:,:,2) - r(:,:,1)) / (ABS(r(:,:,2)) + Small)) < (RelErr * 0.01)) THEN
+        !Recalculate acceleration for corrected position
+        a(:,:,2) = 0
+        DO i = 1,n
+            DO j = 1,n
+            IF (i == j) CYCLE
+                s_sq(i,j) = ((r(1,j,2) - r(1,i,2))**2 + (r(2,j,2) - r(2,i,2))**2 + (r(3,j,2) - r(3,i,2))**2)
 
-            !Double timestep
-            dt = dt * 2.
-
-            !Omit alternate points to adjust timestep
-            DO k = 1,3
-                a(:,:,-k) = a(:,:,-2*k)
-                v(:,:,-k) = v(:,:,-2*k)
+                !Calculate corrected acceleration
+                a(:,i,2) = a(:,i,2) + G * M(j) * (r(:,j,2) - r(:,i,2))*(s_sq(i,j)**-1.5)
             END DO
+        END DO
+
+        !Shift previous values down arrays
+        DO i = -6,0
+            a(:,:,i) = a(:,:,i+1)
+            v(:,:,i) = v(:,:,i+1)
+        END DO
+
+        !Set corrected values as current position, velocity and acceleration
+        r(:,:,0) = r(:,:,2)
+        v(:,:,0) = v(:,:,2)
+        a(:,:,0) = a(:,:,2)
+
+
+        !Ensure enough data points are stored
+        IF (counter(z) > 6) THEN
+            !Check if error from predictor-corrector is small enough
+            IF (errcoeff * MAXVAL(ABS(r(:,:,2) - r(:,:,1)) / (ABS(r(:,:,2)) + Small)) < (RelErr * 0.01)) THEN
+
+                !Double timestep
+                dtmin = dtmin * 2.
+
+                !Omit alternate points to adjust timestep
+                DO k = 1,3
+                    a(:,:,-k) = a(:,:,-2*k)
+                    v(:,:,-k) = v(:,:,-2*k)
+                END DO
+            END IF
+
+            !Check if error from predictor-corrector is too large
+            IF (errcoeff * MAXVAL(ABS(r(:,:,2) - r(:,:,1)) / (ABS(r(:,:,2)) + Small)) > RelErr) THEN
+
+                !Halve timestep
+                dtmin = dtmin * 0.5
+
+                !Interpolate previous data points for predictor-corrector
+                a_int(:,:,1) = (-5. * a(:,:,-4) + 28. * a(:,:,-3) - 70. * a(:,:,-2) + 140. * a(:,:,-1) + 35. * a(:,:,0)) / 128.
+                a_int(:,:,2) = (3. * a(:,:,-4) - 20. * a(:,:,-3) + 90. * a(:,:,-2) + 60. * a(:,:,-1) -5. * a(:,:,0)) / 128.
+
+                v_int(:,:,1) = (-5. * v(:,:,-4) + 28. * v(:,:,-3) - 70. * v(:,:,-2) + 140. * v(:,:,-1) + 35. * v(:,:,0)) / 128.
+                v_int(:,:,2) = (3. * v(:,:,-4) - 20. * v(:,:,-3) + 90. * v(:,:,-2) + 60. * v(:,:,-1) -5. * v(:,:,0)) / 128.
+
+                !Create new mesh
+                !-5 and -6 unimportant, not used in calculation & overwritten before next time-step change
+                a(:,:,-4) = a(:,:,-2)
+                a(:,:,-3) = a_int(:,:,2)
+                a(:,:,-2) = a(:,:,-1)
+                a(:,:,-1) = a_int(:,:,1)
+
+                v(:,:,-4) = v(:,:,-2)
+                v(:,:,-3) = v_int(:,:,2)
+                v(:,:,-2) = v(:,:,-1)
+                v(:,:,-1) = v_int(:,:,1)
+
+
+            END IF
+
+            !Reset counter to let old data fill up
+            counter(z) = 0
         END IF
 
-        !Check if error from predictor-corrector is too large
-        IF (errcoeff * MAXVAL(ABS(r(:,:,2) - r(:,:,1)) / (ABS(r(:,:,2)) + Small)) > RelErr) THEN
-
-            !Halve timestep
-            dt = dt * 0.5
-
-            !Interpolate previous data points for predictor-corrector
-            a_int(:,:,1) = (-5. * a(:,:,-4) + 28. * a(:,:,-3) - 70. * a(:,:,-2) + 140. * a(:,:,-1) + 35. * a(:,:,0)) / 128.
-            a_int(:,:,2) = (3. * a(:,:,-4) - 20. * a(:,:,-3) + 90. * a(:,:,-2) + 60. * a(:,:,-1) -5. * a(:,:,0)) / 128.
-
-            v_int(:,:,1) = (-5. * v(:,:,-4) + 28. * v(:,:,-3) - 70. * v(:,:,-2) + 140. * v(:,:,-1) + 35. * v(:,:,0)) / 128.
-            v_int(:,:,2) = (3. * v(:,:,-4) - 20. * v(:,:,-3) + 90. * v(:,:,-2) + 60. * v(:,:,-1) -5. * v(:,:,0)) / 128.
-
-            !Create new mesh
-            !-5 and -6 unimportant, not used in calculation & overwritten before next time-step change
-            a(:,:,-4) = a(:,:,-2)
-            a(:,:,-3) = a_int(:,:,2)
-            a(:,:,-2) = a(:,:,-1)
-            a(:,:,-1) = a_int(:,:,1)
-
-            v(:,:,-4) = v(:,:,-2)
-            v(:,:,-3) = v_int(:,:,2)
-            v(:,:,-2) = v(:,:,-1)
-            v(:,:,-1) = v_int(:,:,1)
 
 
-        END IF
 
-        !Reset counter to let old data fill up
-        counter = 0
-    END IF
+        counter(z) = counter(z) + 1
 
+        !update that timestep has been reached
+        deltat(z) = 0
+
+    END DO
+
+    deltat(:) = deltat(:) + dtmin
 
 
     !Write every 500th step to log files
     IF ((MOD(stepno, 500) == 0) .and. (lg == 'y')) THEN
+        DO i = 1,n
+            !Calculate absolute distance in each direction for logging files
+            dist(:, i, j) = r(:,j,1) - r(:,i,1)
+        END DO
         WRITE(2,*) time, ',', dist(1,1,1), ',', dist(2,1,1), ',', dist(3,1,1)
         WRITE(3,*) time, ',', dist(1,1,2), ',', dist(2,1,2), ',', dist(3,1,2)
         WRITE(4,*) time, ',', dist(1,1,3), ',', dist(2,1,3), ',', dist(3,1,3)
@@ -383,9 +382,8 @@ END IF
     END IF
 
     !update elapsed time
-    time = time + dt
+    time = time + dtmin
     stepno = stepno + 1
-    counter = counter + 1
     IF (time > (num_yr * 31536000.)) EXIT
 END DO
 
@@ -428,7 +426,7 @@ DO i = 1,n
     END DO
 END DO
 
-WRITE(6,*) dt
+WRITE(6,*) dtmin
 
 !Loop through each body to find GPE of each
 GPE = 0
